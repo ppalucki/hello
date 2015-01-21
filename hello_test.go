b844dd0bb594061acfc31befd27377a17ee5fda4
Pawel Palucki <pawel.palucki@intel.com>
2015-01-21 22:02:13 +0100

    goconvey, testify and gom together

--- hello_test.go
+++ hello_test.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	got := simple()
-	if got != "hello world" {
-		t.Fail()
-	}
+	// got := simple()
+	// if got != "hello world" {
+	// 	t.Fail()
+	// }
+
+	Convey("I want my hw example working", t, func() {
+		So(simple(), ShouldEqual, "hello world")
+	})
+
+	// I like asserts more
+	assert.Equal(t, "hello world", simple())
+}
+
+func TestSkipped(t *testing.T) {
+	t.Skip()
 }
